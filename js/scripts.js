eae894fd7275372f5869c8c158a86d34e8c82b14
Jena SanCartier <jenasancartier@gmail.com>
2016-12-22 14:51:03 -0800

    begin inserting responsive dropdown menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
-
-
+$(document).ready(function() {
+  $('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
 });
