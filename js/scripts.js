f79582ceaae99fa4755e2b59d47e4c727e4f8d4c
Jena SanCartier <jenasancartier@gmail.com>
2017-01-19 16:23:24 -0800

    fix error that was preventing dropdown menu from displaying

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,5 @@ $(document).ready(function() {
   $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
-
-
-
   });
-
-  $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 800);
-    return false;
 });
