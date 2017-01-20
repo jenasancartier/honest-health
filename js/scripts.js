0c7f99a9d1dbc465537439f8952e63c0488a465b
Jena SanCartier <jenasancartier@gmail.com>
2017-01-19 16:50:03 -0800

    remove javascript code that was preventing link from redirecting to new page

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,4 @@ $(document).ready(function() {
    $('#overlay').toggleClass('open');
   });
 
-  $("ul").find("a").click(function(e) {
-      e.preventDefault();
-      var section = $(this).attr("href");
-      $("html, body").animate({
-          scrollTop: $(section).offset().top
-      });
-  });
-
 });
