56c33c7636d2eabbebc4afe9f67634a1f7aa11bd
Jena SanCartier <jenasancartier@gmail.com>
2017-01-19 16:47:35 -0800

    made new page for strains and began to add link to the new page

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+  $("ul").find("a").click(function(e) {
+      e.preventDefault();
+      var section = $(this).attr("href");
+      $("html, body").animate({
+          scrollTop: $(section).offset().top
+      });
+  });
+
 });
